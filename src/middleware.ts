7f344a53dabde7eefb1943a9ef9b0b4370f84032
stephenwayar <stephenbuluswayar@gmail.com>
2024-10-10 14:42:33 +0100

    feat: add authorization middleware funct, add error pages

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { Key } from './redux/types/user.types';
+import { NextResponse, NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  let isLoggedIn = false;
+
+  const cookieUser = request.cookies.get(Key.BUDDY_USER);
+
+  // List of authentication pages
+  const authPages = [
+    '/auth/login', 
+    '/auth/signup',
+    '/auth/signup/with-email',
+  ];
+
+  // Check if the user exists
+  if (cookieUser) {
+    isLoggedIn = true;
+  }
+
+  // Redirect unauthorized users trying to access protected pages
+  if (request.nextUrl.pathname.startsWith('/dashboard') && !isLoggedIn) {
+    return NextResponse.redirect(new URL(`/auth/login?redirect=${request.nextUrl.pathname}`, request.url));
+  }
+
+  // Redirect logged-in users trying to access authentication pages
+  if (authPages.includes(request.nextUrl.pathname) && isLoggedIn) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  // Allow the request to proceed to the next middleware
+  return NextResponse.next();
+}
---
+++ src/pages/404.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+
+export default function PageNotFound() {
+  return (
+    <div className="h-screen flex justify-center items-center px-4 sm:px-6">
+      <div className="space-y-4">
+        <h1 className="text-[#090A04] text-center text-4xl font-semibold">
+          Oops!
+        </h1>
+
+        <h3 className="text-[#090A04] text-center text-lg">
+          The page you are looking for does not exist
+        </h3>
+
+        <div className="text-center">
+          <Link href='/'>
+            <button className="bg-[#FF8600] hover:bg-[#ff8800e1] text-white h-[45px] rounded-lg text-center w-36">
+              Go home
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
